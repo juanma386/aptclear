8c8abaa0598fc92c692aaf3c50b9cca6d0f0de1a
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2018-12-11 11:39:31 -0300

    clean commit initial

---
+++ uninstall-webmin-debian9-clear.sh
@@ -0,0 +1,11 @@
+ apt-get  -y --purge remove webmin
+ apt-get  -y --purge remove virtualmin*
+ apt-get  -y --purge remove apache2*
+ apt-get  -y --purge remove mysql*
+ apt-get  -y --purge remove php7*
+ apt-get  -y --purge remove php5*
+ apt-get  -y --purge remove bind*
+ aptitude remove -y postfix* --purge-unused
+ aptitude remove -y postfix --purge-unused
+ aptitude remove -y spamassissin --purge-unused
+ 
